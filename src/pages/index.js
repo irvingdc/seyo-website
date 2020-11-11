16f4dfbc89c37ccf7a519e24157add84d8c8ab11
irvingdc <irvingedc@gmail.com>
2020-11-11 14:41:47 -0600

    added banner for buen fin and discount on checkout page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react"
+import React, { Fragment, useState, useEffect } from "react"
 import "stylesheets/main.module.less"
 import "bootstrap/dist/css/bootstrap.min.css"
 import Layout from "components/Layout/Layout"
@@ -8,105 +8,133 @@ import HomeLocks from "components/HomeLocks/HomeLocks"
 import img1 from "images/banners/freya_home.jpg"
 import img2 from "images/banners/not_home.jpg"
 import img3 from "images/banners/syn_home.jpg"
+import buenfin from "images/banners/buenfin.jpg"
 import otros_productos from "images/banners/otros_productos.png"
 import locks_all from "images/banners/locks_all_cropped.jpg"
 import classes from "stylesheets/index.module.less"
 import { getPriceString } from "utils/functions"
 import { Link } from "gatsby"
 import { addToCart } from "components/Cart/Cart"
+import Modal from "react-bootstrap/Modal"
 
-const IndexPage = () => (
-  <Layout
-    title="Seyo México"
-    description="Abre la puerta al futuro, abre la puerta a SEYO."
-    canonical="/"
-  >
-    <div className={classes.carousel}>
-      <Carousel
-        items={[
-          <LockBanner
-            img={img1}
-            label="Cerradura Inteligente"
-            title="FREYA19"
-            price={getPriceString("FREYA19")}
-            actions={[
-              {
-                title: "CONOCE MÁS",
-                url: "/cerraduras/freya19",
-              },
-              {
-                title: "COMPRA YA",
-                onClick: () => addToCart("FREYA19"),
-              },
-            ]}
-          />,
-          <LockBanner
-            img={img2}
-            label="Cerradura Inteligente"
-            title="NOT19"
-            price={getPriceString("NOT19")}
-            actions={[
-              {
-                title: "CONOCE MÁS",
-                url: "/cerraduras/not19",
-              },
-              {
-                title: "COMPRA YA",
-                onClick: () => addToCart("NOT19"),
-              },
-            ]}
-          />,
-          <LockBanner
-            img={img3}
-            label="Cerradura Inteligente"
-            title="SYN19"
-            price={getPriceString("SYN19")}
-            actions={[
-              {
-                title: "CONOCE MÁS",
-                url: "/cerraduras/syn19",
-              },
-              {
-                title: "COMPRA YA",
-                onClick: () => addToCart("SYN19"),
-              },
-            ]}
-          />,
+export const useLocalStorage = (key, defaultValue) => {
+  const stored = window.localStorage.getItem(key)
+  const initial = stored ? JSON.parse(stored) : defaultValue
+  const [value, setValue] = useState(initial)
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      window.localStorage.setItem(key, JSON.stringify(value))
+    }
+  }, [key, value])
+
+  return [value, setValue]
+}
+
+const IndexPage = () => {
+  let [hidden, setHidden] = useLocalStorage("modal.buenfin.hidden", false)
+
+  return (
+    <Layout
+      title="Seyo México"
+      description="Abre la puerta al futuro, abre la puerta a SEYO."
+      canonical="/"
+    >
+      <div className={classes.carousel}>
+        <Modal
+          show={!hidden}
+          onHide={() => setHidden(true)}
+          dialogClassName={classes.modalClass}
+        >
+          <Modal.Body>
+            <img src={buenfin} alt="buen fin" />
+          </Modal.Body>
+        </Modal>
+        <Carousel
+          items={[
+            <LockBanner
+              img={img1}
+              label="Cerradura Inteligente"
+              title="FREYA19"
+              price={getPriceString("FREYA19")}
+              actions={[
+                {
+                  title: "CONOCE MÁS",
+                  url: "/cerraduras/freya19",
+                },
+                {
+                  title: "COMPRA YA",
+                  onClick: () => addToCart("FREYA19"),
+                },
+              ]}
+            />,
+            <LockBanner
+              img={img2}
+              label="Cerradura Inteligente"
+              title="NOT19"
+              price={getPriceString("NOT19")}
+              actions={[
+                {
+                  title: "CONOCE MÁS",
+                  url: "/cerraduras/not19",
+                },
+                {
+                  title: "COMPRA YA",
+                  onClick: () => addToCart("NOT19"),
+                },
+              ]}
+            />,
+            <LockBanner
+              img={img3}
+              label="Cerradura Inteligente"
+              title="SYN19"
+              price={getPriceString("SYN19")}
+              actions={[
+                {
+                  title: "CONOCE MÁS",
+                  url: "/cerraduras/syn19",
+                },
+                {
+                  title: "COMPRA YA",
+                  onClick: () => addToCart("SYN19"),
+                },
+              ]}
+            />,
+          ]}
+        />
+      </div>
+      <div className={classes.locksBanner}>
+        <Link to="/cerraduras">
+          <div>
+            <h2>
+              Cerraduras Inteligentes <b>SEYO</b>
+            </h2>
+            <h3>
+              Desde <b>$3,580.00</b>
+            </h3>
+          </div>
+          <img src={locks_all} alt="Nuestras Cerraduras" />
+        </Link>
+      </div>
+      <HomeLocks />
+      <LockBanner
+        img={otros_productos}
+        title={<span className={classes.finalSubtitle}>Otros Productos</span>}
+        darkText
+        price={
+          <Fragment>
+            Desde <b>$3,580.00</b>
+          </Fragment>
+        }
+        actions={[
+          {
+            title: "CONOCE MÁS",
+            url: "/otros-productos",
+            blue: true,
+          },
         ]}
       />
-    </div>
-    <div className={classes.locksBanner}>
-      <Link to="/cerraduras">
-        <div>
-          <h2>
-            Cerraduras Inteligentes <b>SEYO</b>
-          </h2>
-          <h3>
-            Desde <b>$3,580.00</b>
-          </h3>
-        </div>
-        <img src={locks_all} alt="Nuestras Cerraduras" />
-      </Link>
-    </div>
-    <HomeLocks />
-    <LockBanner
-      img={otros_productos}
-      title={<span className={classes.finalSubtitle}>Otros Productos</span>}
-      darkText
-      price={
-        <Fragment>
-          Desde <b>$3,580.00</b>
-        </Fragment>
-      }
-      actions={[
-        {
-          title: "CONOCE MÁS",
-          url: "/otros-productos",
-          blue: true,
-        },
-      ]}
-    />
-  </Layout>
-)
-
+    </Layout>
+  )
+}
 export default IndexPage
