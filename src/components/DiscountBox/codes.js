876570537a92b2a476da905b29e36693d695aaab
irvingdc <irvingedc@gmail.com>
2021-04-23 12:43:19 -0500

    extended coupon code expiration

--- src/components/DiscountBox/codes.js
+++ src/components/DiscountBox/codes.js
@@ -1,10 +1,10 @@
 export default {
   "6c91c99f18cd12241dc3e95c4ddee29d260a44bd": {
     discount: 0.15,
-    expiration: "2021-05-05",
+    expiration: "2021-07-05",
   },
   "e3dae3873812e9728b078498c4cafa23a8577bba": {
     discount: 0.15,
-    expiration: "2021-05-05",
+    expiration: "2021-07-05",
   },
 }
