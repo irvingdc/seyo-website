eb29a3c17a5ab499b49a08e0e537aee66ca5f66d
Irving Diaz <irvingdiaz@Irvings-MacBook-Pro.local>
2021-11-01 17:54:58 -0600

    syn google kit is temporarily hidden

--- src/pages/kits-de-automatizacion.js
+++ src/pages/kits-de-automatizacion.js
@@ -13,57 +13,58 @@ import banner1 from "images/accesories/KIT_GOOGLE_SYN21/banner.jpg"
 import banner2 from "images/accesories/KIT_GOOGLE_TYR21/banner.jpg"
 
 export default () => (
-    <Layout
-        title="Seyo | Kits de Automatización"
-        description="Da un paso más hacia la automatización doméstica."
-        canonical="/kits-de-automatizacion/"
-    >
-        <div className={classes.container}>
-
-            <div>
-                <h1>Kits de Automatización</h1>
-                <Carousel
-                    items={[
-                        <LockBanner
-                            img={banner1}
-                            title={<ul>
-                                <li>Cerradura SYN21</li>
-                                <li>Google Nest Mini</li>
-                                <li>Gateway</li>
-                                <li>Envío Gratis</li>
-                            </ul>}
-                            price={getPriceString("KIT_GOOGLE_SYN21")}
-                            actions={[
-                                {
-                                    title: "CONOCE MÁS",
-                                    url: "/otros-productos/kit-google-syn21/",
-                                },
-                            ]}
-                        />,
-                        <LockBanner
-                            img={banner2}
-                            title={<ul>
-                                <li>Cerradura TYR21</li>
-                                <li>Google Nest Mini</li>
-                                <li>Gateway</li>
-                                <li>Envío Gratis</li>
-                            </ul>}
-                            price={getPriceString("KIT_GOOGLE_TYR21")}
-                            actions={[
-                                {
-                                    title: "CONOCE MÁS",
-                                    url: "/otros-productos/kit-google-tyr21/",
-                                },
-                            ]}
-                        />,
-                    ]}
-                />
-            </div>
-            <h2>
-                Protege lo que más te importa con <b>SEYO</b>.
-            </h2>
-            <div className={classes.allLocks}>
-                <LockCard
+  <Layout
+    title="Seyo | Kits de Automatización"
+    description="Da un paso más hacia la automatización doméstica."
+    canonical="/kits-de-automatizacion/"
+  >
+    <div className={classes.container}>
+      <div>
+        <h1>Kits de Automatización</h1>
+        {/*<LockBanner
+            img={banner1}
+            title={<ul>
+                <li>Cerradura SYN21</li>
+                <li>Google Nest Mini</li>
+                <li>Gateway</li>
+                <li>Envío Gratis</li>
+            </ul>}
+            price={getPriceString("KIT_GOOGLE_SYN21")}
+            actions={[
+                {
+                    title: "CONOCE MÁS",
+                    url: "/otros-productos/kit-google-syn21/",
+                },
+            ]}
+        />,*/}
+        <Carousel
+          items={[
+            <LockBanner
+              img={banner2}
+              title={
+                <ul>
+                  <li>Cerradura TYR21</li>
+                  <li>Google Nest Mini</li>
+                  <li>Gateway</li>
+                  <li>Envío Gratis</li>
+                </ul>
+              }
+              price={getPriceString("KIT_GOOGLE_TYR21")}
+              actions={[
+                {
+                  title: "CONOCE MÁS",
+                  url: "/otros-productos/kit-google-tyr21/",
+                },
+              ]}
+            />,
+          ]}
+        />
+      </div>
+      <h2>
+        Protege lo que más te importa con <b>SEYO</b>.
+      </h2>
+      <div className={classes.allLocks}>
+        {/*<LockCard
                     subtitle="Kit De Apertura Remota"
                     link="/otros-productos/kit-google-syn21/"
                     direction="left"
@@ -71,17 +72,17 @@ export default () => (
                     img={syn}
                     price={getPriceString("KIT_GOOGLE_SYN21")}
                     methods={[]}
-                />
-                <LockCard
-                    subtitle="Kit De Apertura Remota"
-                    link="/otros-productos/kit-google-tyr21/"
-                    direction="right"
-                    title="Google Home + TYR21"
-                    img={tyr}
-                    price={getPriceString("KIT_GOOGLE_TYR21")}
-                    methods={[]}
-                />
-            </div>
-        </div>
-    </Layout>
+                />*/}
+        <LockCard
+          subtitle="Kit De Apertura Remota"
+          link="/otros-productos/kit-google-tyr21/"
+          direction="right"
+          title="Google Home + TYR21"
+          img={tyr}
+          price={getPriceString("KIT_GOOGLE_TYR21")}
+          methods={[]}
+        />
+      </div>
+    </div>
+  </Layout>
 )
